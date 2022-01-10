c8386015be334c22c829f65c431f725725dfa601
Robert Reinhart <robertreinhart24@gmail.com>
2022-01-10 05:24:48 -0500

    cors-proxy Rev1.0

---
+++ pages/api/index.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+export default function handler(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  request.get(
+    { url: req.headers['my-url'] },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+     
+      res.json(JSON.parse(body));
+    }
+  )
+}
