24470ea253abe2bd308350bd2cadf083e680cea9
Robert Reinhart <46613248+robertreinhart@users.noreply.github.com>
2022-03-12 12:00:09 -0500

    Updated to include url in body

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,10 +1,19 @@
+const express = require('express');
+const request = require('request');
+
 export default function handler(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   request.get(
-    { url: req.body["my-url"] },
+    { url: req.body['my-url'] },
     (error, response, body) => {
+      console.log(body)
       if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: error.message });
+        console.log("__________")
+        if (error) {
+          return res.status(500).json({ type: 'error', message: error.message });
+        } else {
+          return res.status(response.statusCode).json({ type: 'error', message: response.statusMessage });
+        }
       }
 
       res.json(JSON.parse(body));
