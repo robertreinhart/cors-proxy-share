80cafcc780891d7f6c5ff375332eda445827d087
Robert Reinhart <robertreinhart24@gmail.com>
2022-03-12 15:08:26 -0500

    Updated to Axios

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,14 +1,21 @@
+const express = require('express');
 const request = require('request');
 
 export default function handler(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   request.get(
-    { url: req.headers['my-url'] },
+    { url: req.body['my-url'] },
     (error, response, body) => {
+      console.log(body)
       if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: error.message });
+        console.log("__________")
+        if (error) {
+          return res.status(500).json({ type: 'error', message: error.message });
+        } else {
+          return res.status(response.statusCode).json({ type: 'error', message: response.statusMessage });
+        }
       }
-     
+
       res.json(JSON.parse(body));
     }
   )
