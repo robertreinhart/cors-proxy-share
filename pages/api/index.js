217aba009172fa0f20972abba7f5f14c764b2957
Robert Reinhart <robertreinhart24@gmail.com>
2023-08-13 18:37:09 -0400

    fix proxy for retail tracer

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,27 @@
 const axios = require('axios');
 
 export default function handler(req, res, next) {
+  let x=0;
+  let  success = false;
   async function getURI(url) {
+   do {
+      console.log(x)
     try {
       const response = await axios.get(url);
-      console.log(response)
       if (response.status !== 200) {
         return res.status(response.status).json({ type: 'error', message: response.statusText });
       } else {
-      res.json(response.data);
+      success = true
+      res.json(JSON.stringify(response.data))
       }
     } catch (error) {
-      console.log(error)
-      return res.status(500).json({ type: 'error', message: error.message });
+      x++
+      console.log(error.message, "ERR")
+      // return res.status(500).json({ type: 'error', message: error.message });
     }
+  } while (!success && x<100)
   }
   res.setHeader('Access-Control-Allow-Origin', '*');
-  getURI(req.body['my-url'])
+  res.setHeader('Content-Type', 'application/json');
+  getURI(JSON.parse(req.body)['my-url'])
 }
