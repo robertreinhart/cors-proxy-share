c07a4c7d4c9342f5671d262c3401d885dc67402a
Robert Reinhart <robertreinhart24@gmail.com>
2025-12-12 14:49:22 -0500

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,17 +4,17 @@ export default function handler(req, res, next) {
   async function getURI(url) {
     try {
       const response = await axios.get(url);
+      console.log(response)
       if (response.status !== 200) {
         return res.status(response.status).json({ type: 'error', message: response.statusText });
       } else {
-      res.json(JSON.stringify(response.data))
+      res.json(response.data);
       }
     } catch (error) {
-      console.log(error.message, "ERR")
-      // return res.status(500).json({ type: 'error', message: error.message });
+      console.log(error)
+      return res.status(500).json({ type: 'error', message: error.message });
     }
-  } 
+  }
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  getURI(JSON.parse(req.body)['my-url'])
+    getURI(req.body['my-url'])
 }
