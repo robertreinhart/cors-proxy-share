103e96da99f453bf8fdcbf8691780403e71075ac
Robert Reinhart <robertreinhart24@gmail.com>
2025-12-12 15:16:44 -0500

    Updated header

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,17 +4,17 @@ export default function handler(req, res, next) {
   async function getURI(url) {
     try {
       const response = await axios.get(url);
-      console.log(response)
+    
       if (response.status !== 200) {
         return res.status(response.status).json({ type: 'error', status:response.status, message: response.statusText });
       } else {
       res.json(response.data);
       }
     } catch (error) {
-      console.log(error)
+ 
       return res.status(500).json({ type: 'error', status:"500", message: error.message });
     }
   }
   res.setHeader('Access-Control-Allow-Origin', '*');
-    getURI(req.body['my-url'])
+    getURI(req.headers['my-url'])
 }
