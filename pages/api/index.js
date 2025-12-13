b2e6927585a1b6546b9fa1e1c8868cf4ed28b196
Robert Reinhart <robertreinhart24@gmail.com>
2025-12-12 19:13:15 -0500

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,27 @@
-const axios = require('axios');
+const axios = require("axios");
 
 export default function handler(req, res, next) {
+  console.log(req.headers["my-url"]);
   async function getURI(url) {
     try {
       const response = await axios.get(url);
-    
       if (response.status !== 200) {
-        return res.status(response.status).json({ type: 'error', status:response.status, message: response.statusText });
+        return res.status(response.status).json({
+          type: "error",
+          status: response.status,
+          message: response.statusText,
+        });
       } else {
-      res.json(response.data);
+        return res.json(response.data);
       }
     } catch (error) {
- 
-      return res.status(500).json({ type: 'error', status:"500", message: error.message });
+      console.log(error.response)
+      return res.status(500).json({ type: "error", message: error.message });
     }
   }
-  res.setHeader('Access-Control-Allow-Origin', '*');
-    getURI(req.headers['my-url'])
+  res.setHeader("Access-Control-Allow-Origin", "*", {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+  });
+  getURI(req.headers["my-url"]);
 }
